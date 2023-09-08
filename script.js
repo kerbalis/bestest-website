16ae3017ad7e71ef36258aac5bffe077a33cca07
kerbalis <129982208+kerbalis@users.noreply.github.com>
2023-09-08 07:21:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-// JavaScript code to add flying balls dynamically
-
-const ballContainer = document.querySelector(".ball-container");
-
-function createBall() {
-    const ball = document.createElement("div");
-    ball.classList.add("ball");
-    ball.style.left = `${Math.random() * 100}vw`;
-    ball.style.top = `${Math.random() * 100}vh`;
-    ballContainer.appendChild(ball);
-}
 
 document.getElementById("doNothingButton").addEventListener("click", () => {
     // This button does nothing
 });
-
-// Create initial balls
-for (let i = 0; i < 50; i++) {
-    createBall();
-}
